92251a10ba31a851a821297968f6f54ac0f1ae47
bagelcow <102549803+bagelcow@users.noreply.github.com>
2022-03-29 20:05:16 +0100

    test

--- theme.config.js
+++ theme.config.js
@@ -5,12 +5,9 @@ export default {
     <small style={{ display: 'block', marginTop: '8rem' }}>
       <time>{YEAR}</time> © Leks' Thoughts
       <a href="/feed.xml">RSS</a>
-      <a href="mailto:hello@leks.gg">Contact me</a><span class="1"> | </span>
+      <a href="mailto:hello@leks.gg">Contact me</a>
       <style jsx>{`
         a {
-          float: right;
-        }
-        1 {
           float: right;
         }
         @media screen and (max-width: 480px) {
