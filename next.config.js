ba6bd1f4f99cde23e4025a535aeef4e977223d16
bagelcow <102549803+bagelcow@users.noreply.github.com>
2022-04-21 20:51:34 +0100

    add discord url

--- next.config.js
+++ next.config.js
@@ -1,2 +1,14 @@
 const withNextra = require('nextra')('nextra-theme-blog', './theme.config.js')
 module.exports = withNextra()
+
+module.exports = {
+    async redirects() {
+        return [
+            {
+                source: '/enfim',
+                destination: 'https://discord.gg/yc9qtFc',
+                permanent: false
+            }
+        ];
+    }
+};
